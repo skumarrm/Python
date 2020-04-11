0b5167d2eb5d5a43fe08c8c11ed9a2847b5971dd
Senthil <mssenthil87@gmail.com>
2020-04-11 19:21:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   profile    = "default"
-  region     = "${var.region}"
+  region     = var.region
 }
 
 
 resource "aws_instance" "myFirst_Terraform_Instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  tags {
-        Name = "${var.ec2-instance-name}"
-	   }
+  tags = {
+        Name = var.ec2-instance-name
+       }
 }
